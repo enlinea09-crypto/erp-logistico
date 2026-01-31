e6b940ed8d7ddf118eccd9d5255d3960364aa25b
ENRIQUE <enlinea09@gmail.com>
2026-01-31 12:36:06 -0600

    fix test route registration

--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -1,15 +1,8 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const verifyToken = require("../middlewares/auth.middleware")
+router.get("/", (req, res) => {
+  res.json({ message: "API ERP funcionando OK" });
+});
 
-router.get("/protected", verifyToken, (req, res) => {
-
- res.json({
-  message: "Acceso autorizado",
-  userId: req.userId
- })
-
-})
-
-module.exports = router
+module.exports = router;
