f7e88138f679613ed29481746dcdc421e95149d1
ENRIQUE <enlinea09@gmail.com>
2026-01-31 09:46:23 -0600

    Add missing auth middleware

--- backend/src/routes/kpi.routes.js
+++ backend/src/routes/kpi.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getDashboardKPI } = require("../controllers/kpi.controller");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.get("/dashboard", authMiddleware, getDashboardKPI);
 
